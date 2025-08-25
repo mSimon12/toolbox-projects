0f9a35b34d6135f63ec7799d697e19a01ea3f278
mSimon12 <marcelo_simon@hotmail.com>
2025-08-25 21:53:08 +0200

    feat: clean code and configure safe infinite loop

--- projects/sys-monitor/main.go
+++ projects/sys-monitor/main.go
@@ -1,73 +1,83 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"flag"
 	"fmt"
 	"log/slog"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 )
 
 const size1GB float64 = 1073741824
 
-func getSystemMetrics(outputFile string) {
-	var metricsLogger *slog.Logger
-	if outputFile == "" {
-		metricsLogger = GetLogger(false, outputFile)
-	} else {
-		if !strings.HasSuffix(outputFile, ".log") {
-			outputFile = outputFile + ".log"
-		}
-		metricsLogger = GetLogger(true, outputFile)
+func formatOutputFile(filename string) string {
+	if !strings.HasSuffix(filename, ".log") {
+		filename = filename + ".log"
 	}
+	return filename
+}
 
-	// CPU
-	cpuReport, _ := getCpuUsage(3)
+func logCpuUsage(logger *slog.Logger, usageReport cpuUsage) {
 	fields := []interface{}{
-		"total", fmt.Sprintf("%.2f%%", cpuReport.total),
+		"total", fmt.Sprintf("%.2f%%", usageReport.total),
 	}
 
-	for idx, core := range cpuReport.cores {
+	for idx, core := range usageReport.cores {
 		key := fmt.Sprintf("core%d", idx+1)
 		value := fmt.Sprintf("%.2f%%", core)
 		fields = append(fields, key, value)
 	}
-	metricsLogger.Info("CPU", fields...)
+	logger.Info("CPU", fields...)
+}
 
-	// Memory
-	memReport, _ := getMemoryUsage()
-	metricsLogger.Info("Memory",
-		"total", fmt.Sprintf("%.2f GB", float64(memReport.total)/size1GB),
-		"free", fmt.Sprintf("%.2f GB", float64(memReport.free)/size1GB),
-		"used", fmt.Sprintf("%.2f GB", float64(memReport.used)/size1GB),
-		"percent", fmt.Sprintf("%.2f%%", memReport.percent),
-	)
-
-	// Disk
-	diskReport, _ := getDiskUsage()
-	metricsLogger.Info("Disk",
-		"total", fmt.Sprintf("%.2f GB", float64(diskReport.total)/size1GB),
-		"free", fmt.Sprintf("%.2f GB", float64(diskReport.free)/size1GB),
-		"used", fmt.Sprintf("%.2f GB", float64(diskReport.used)/size1GB),
-		"percent", fmt.Sprintf("%.2f%%", diskReport.percent),
+func logCommonUsage(logger *slog.Logger, message string, usageReport Usage) {
+	logger.Info(message,
+		"total", fmt.Sprintf("%.2f GB", float64(usageReport.total)/size1GB),
+		"free", fmt.Sprintf("%.2f GB", float64(usageReport.free)/size1GB),
+		"used", fmt.Sprintf("%.2f GB", float64(usageReport.used)/size1GB),
+		"percent", fmt.Sprintf("%.2f%%", usageReport.percent),
 	)
 }
 
-func metricsCycle(interval int, outputFile string) {
+func getSystemMetrics() (cpuUsage, Usage, Usage) {
+	cpuReport, _ := getCpuUsage(3)
+	memReport, _ := getMemoryUsage()
+	diskReport, _ := getDiskUsage()
+
+	return cpuReport, memReport, diskReport
+}
+
+func metricsCycle(ctx context.Context, logger *slog.Logger, interval int) {
 	count := 0
-	for count < 2 {
-		getSystemMetrics(outputFile)
-		time.Sleep(time.Duration(interval))
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+	for {
+
+		select {
+		case <-ticker.C:
+			cpu, memory, disk := getSystemMetrics()
+			logCpuUsage(logger, cpu)
+			logCommonUsage(logger, "Memory", memory)
+			logCommonUsage(logger, "Disk", disk)
+		case <-ctx.Done():
+			fmt.Println("stopping PeriodicTask")
+			ticker.Stop()
+			return
+		}
+
+		time.Sleep(time.Duration(interval) * time.Second)
 		count++
 	}
-
 }
 
 func run() error {
 	var interval int
 	var outputFile string
+	ctx := context.Background()
 
 	flag.IntVar(&interval, "interval", 5, "Interval (s) between metrics measurements.")
 	flag.StringVar(&outputFile, "outputFile", "", "Name of the file to save the output [Empty = cmd output].")
@@ -76,7 +86,17 @@ func run() error {
 	if interval < 1 {
 		return errors.New("invalid interval: must be > 0")
 	}
-	metricsCycle(interval, outputFile)
+
+	metricsLogger := GetLogger(outputFile != "", formatOutputFile(outputFile))
+	go metricsCycle(ctx, metricsLogger, interval)
+
+	// Create a channel to receive signals from the operating system.
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGTERM)
+
+	// The code blocks until a signal is received (e.g. Ctrl+C).
+	<-sigCh
+
 	return nil
 }
 

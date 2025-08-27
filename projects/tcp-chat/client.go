6a08a0038a08922c28b86413b22b5958c1f02c58
mSimon12 <marcelo_simon@hotmail.com>
2025-08-27 22:46:23 +0200

    feat: persistent client with response  handler

--- projects/tcp-chat/client.go
+++ projects/tcp-chat/client.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"net"
+	"os"
+	"strings"
 )
+
+func handleResponses(tcpConn net.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := tcpConn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Server disconnected:", tcpConn.RemoteAddr())
+			} else {
+				fmt.Println("Read error:", err)
+			}
+			return
+		}
+
+		msg := string(buf[:n])
+		fmt.Printf("\n(%s)\n%s\n\t\t\t ", tcpConn.RemoteAddr(), msg)
+	}
+
+}
 
 func Client(serverAddress string) error {
 	tcpConn, err := net.Dial("tcp", serverAddress)
@@ -12,12 +35,24 @@ func Client(serverAddress string) error {
 		return fmt.Errorf("failed connecting: %w", err)
 	}
 	defer tcpConn.Close()
+	fmt.Println("Connected to Server", tcpConn.RemoteAddr())
 
-	msg := []byte("Hello from Client")
+	go handleResponses(tcpConn)
 
-	if _, err := tcpConn.Write(msg); err != nil {
-		return errors.New("failed to send message")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("\n\t\t\t ")
+		cmdInput, _ := reader.ReadString('\n')
+		cmdInput = strings.TrimSpace(cmdInput) // Remove any surrounding whitespace including the newline.
+
+		if strings.Compare(cmdInput, "/exit") == 0 {
+			return nil
+		}
+
+		msg := []byte(cmdInput)
+
+		if _, err := tcpConn.Write(msg); err != nil {
+			return errors.New("failed to send message")
+		}
 	}
-
-	return nil
 }
